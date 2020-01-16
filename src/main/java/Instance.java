57cd4bd8749b40a88cf541ba5a5ea0e919a88a4d
Kevin Roy <kevin.roy2@etu.univ-nantes.fr>
2020-01-16 20:41:17 +0100

    Add myAlgo

--- src/main/java/Instance.java
+++ src/main/java/Instance.java
@@ -57,16 +57,38 @@ public class Instance {
         clearMachines();
         assignTask();
         System.out.println("Résultat LSA = "+ machineWithMoreTask(this.M));
-        System.out.println("ration LSA = ");
+        System.out.println("ration LSA = "+(machineWithMoreTask(this.M).getCumTask()/(sumOfTask()/this.M.length)));
     }
 
     public void largestProcessingTime(){
         clearMachines();
-        long debut = System.currentTimeMillis();
         copyOfSortedD(this.D);
         assignTask();
         System.out.println("Résultat LPT = "+machineWithMoreTask(this.M));
-        System.out.println("ration LPT = ");
+        System.out.println("ration LPT = "+(machineWithMoreTask(this.M).getCumTask()/(sumOfTask()/this.M.length)));
+    }
+
+    public void myAlgo(){
+        clearMachines();
+        int bound = (sumOfTask()/this.M.length);
+        int j = 0;
+        //Fill machine with task until theoric bound has been reached.
+        for(int i=0;i<this.M.length;i++){
+            while(j<D.length && M[i].getCumTask()<bound){
+                if(D[j]+M[i].getCumTask()<=bound){
+                    M[i].addTask(D[j]);
+                }
+                j++;
+            }
+        }
+        //If all task where not added because solution can't be perfectly fit with the theoric bound.
+        for (int i = j; i < D.length; i++) {
+            machineWithLessTask(this.M).addTask(D[i]);
+        }
+
+        System.out.println("Résultat MyAlgo = "+machineWithMoreTask(this.M));
+        System.out.println("ration MyAlgo = "+(machineWithMoreTask(this.M).getCumTask()/(sumOfTask()/this.M.length)));
+
     }
 
     /**
@@ -121,5 +143,13 @@ public class Instance {
         for (int i = 0; i<M.length; i++) {
             this.M[i] = new Machine();
         }
+    }
+
+    private int sumOfTask(){
+        int res=0;
+        for(int i=0;i<this.D.length;i++){
+            res+=D[i];
+        }
+        return res;
     }
 }
